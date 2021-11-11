557678cd8c7a207a1ac637fc0de851f73fe519ee
IvanBeker <4141zx@gmail.com>
2021-11-11 15:15:57 +0200

    initial commit

---
+++ src/main/java/com/lemonado/smartmeet/SmartMeetServerApplication.java
@@ -0,0 +1,13 @@
+package com.lemonado.smartmeet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartMeetServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartMeetServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lemonado/smartmeet/SmartMeetServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lemonado.smartmeet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartMeetServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
