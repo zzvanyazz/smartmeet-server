0e12a8e5492b652ef9f644bab93b93ecb33f3424
Ivan Beker <ivan.beker@magnise.com.ua>
2021-11-18 10:26:05 +0200

    wip

---
+++ src/main/java/com/lemonado/smartmeet/configs/ServerConfig.java
@@ -0,0 +1,9 @@
+package com.lemonado.smartmeet.configs;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan({"com.lemonado.smartmeet"})
+public class ServerConfig {
+}
